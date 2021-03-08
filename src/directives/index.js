11afa1c6e5a0900d33216b136c56d061ce74560d
shaojihao <1554138768@qq.com>
2021-03-08 15:38:54 +0800

    添加防抖

--- src/directives/index.js
+++ src/directives/index.js
@@ -4,15 +4,16 @@
  * @Author: shaojihao
  */
 
+import _lodash from 'lodash';
 import { portLogInfo } from '@/api/index.js';
 
 // 元素点击埋点 v-logc
 export const logc = {
   bind: function(el, binding) {
-    el.addEventListener('click', () => {
+    el.addEventListener('click', _lodash.debounce(() => {
       // 生产环境提交埋点数据
       if (process.env.NODE_ENV === 'production') portLogInfo(JSON.stringify(binding.value));
-    })
+    }, 500));
   }
 };
 
